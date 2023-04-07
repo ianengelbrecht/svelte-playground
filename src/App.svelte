4ede8c38ce012a4a942c4f10d3fcd792f684f6f9
Ian Engelbrecht <ianicus.za@gmail.com>
2023-04-07 20:51:01 +0200

    backup

--- src/App.svelte
+++ src/App.svelte
@@ -19,7 +19,6 @@
   <div class="card">
     <Counter />
   </div>
-
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
